b901937a0ea2c891bbf419e0d5686ed747628646
carferguz <carferguz@gmail.com>
2022-08-24 20:21:53 -0500

    commit 24082022 Carlos Guzman

--- src/main/java/com/contablebasic/ciclo3/Contablebasic.java
+++ src/main/java/com/contablebasic/ciclo3/Contablebasic.java
@@ -2,6 +2,6 @@ package com.contablebasic.ciclo3;
 
 public class Contablebasic {
     public static void main(String[] args){
-        System.out.println("Metodo Main del projecto 3");
+        System.out.println("Metodo Main del projecto 4");
     }
 }
