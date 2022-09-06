4efde82ea79d7cd4b8a9fb789bcc16d9426a8caa
Alejandro Rojas Tovar <arborvite@gmail.com>
2022-09-06 09:20:58 -0500

    Creación de la Rama development con la configuración de la herramienta Sprint Boot con los paquetes services, controller y domain.

---
+++ ContabilidadBasicaSpringBoot/src/main/java/UdeadByteBEst/ContabilidadBasicaSpringBoot/ContabilidadBasicaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package UdeadByteBEst.ContabilidadBasicaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilidadBasicaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilidadBasicaSpringBootApplication.class, args);
+	}
+
+}
---
+++ ContabilidadBasicaSpringBoot/src/test/java/UdeadByteBEst/ContabilidadBasicaSpringBoot/ContabilidadBasicaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package UdeadByteBEst.ContabilidadBasicaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabilidadBasicaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
